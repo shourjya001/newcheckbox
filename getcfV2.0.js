e9a8562370e0d0940fe1f9ce1d52d2d66d57cce1
shourjya001 <83701525+shourjya001@users.noreply.github.com>
2024-12-31 00:08:00 +0530

    Update getcfV2.0.js

--- getcfV2.0.js
+++ getcfV2.0.js
@@ -1,12 +1,17 @@
 function getCFdetails(codstatus, isOnlyCurrency) {
+    var type = '';
     var searchString = '';
-    var selectSgrCode = document.getElementById("selectsgr_code");
-    var selectLeCode = document.getElementById("selectle_code");
     
+    // Get the value of the select element for SGR code
+    var selectSgrCode = document.getElementById("selectsgr_code");
     if (selectSgrCode && selectSgrCode.value !== '') {
         searchString = selectSgrCode.value;
-    } else if (selectLeCode && selectLeCode.value !== '') {
-        searchString = selectLeCode.value;
+    } else {
+        // Get the value of the select element for LE code
+        var selectLeCode = document.getElementById("selectle_code");
+        if (selectLeCode && selectLeCode.value !== '') {
+            searchString = selectLeCode.value;
+        }
     }
 
     if (searchString !== '') {
